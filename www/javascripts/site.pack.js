c153da72e88d59094e0421a4ef9489e62fac30ee
Walid <hello@walidvb.com>
2019-03-11 02:56:16 +0100

    fixes photo going last

--- www/javascripts/site.pack.js
+++ www/javascripts/site.pack.js
@@ -13,19 +13,20 @@ visiters.forEach(node => {
   if(node.pathname != window.location.pathname){
     const url = node.href;
     const delayVisit = (evt) => {
-    evt.preventDefault();
-    document.body.classList.add('out');
-
-    const fadeUps = document.querySelectorAll('.fade-up');
-    let fadedUp = 0;
+      node.parentNode.classList.add('clicked')
+      evt.preventDefault();
+      document.body.classList.add('out');
+      
+      const fadeUps = document.querySelectorAll('.fade-up');
+      let fadedUp = 0;
       fadeUps.forEach(elem => {
-      elem.addEventListener('animationend', () => {
-        if (++fadedUp == fadeUps.length){
-          setTimeout(() => window.location = url, 50)
-        }
+        elem.addEventListener('animationend', () => {
+          if (++fadedUp == fadeUps.length){
+            setTimeout(() => window.location = url, 50)
+          }
+        })
       })
-    })
-  }
+    }
     node.addEventListener('click', delayVisit)
   }
 })
