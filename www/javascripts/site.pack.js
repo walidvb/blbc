c6f0cf077307be44914045e6b7fbc161b4d76cf5
Walid <hello@walidvb.com>
2019-03-11 02:00:47 +0100

    animates transitions

--- www/javascripts/site.pack.js
+++ www/javascripts/site.pack.js
@@ -7,3 +7,29 @@
 // import './fileName'
 //
 // To learn more, visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+
+const visiters = document.querySelectorAll('.visit-trigger');
+console.log(visiters);
+visiters.forEach(node => {
+  const url = node.attributes.href.value;
+  const delayVisit = (evt) => {
+    evt.preventDefault();
+    document.body.classList.add('out');
+
+    const photo = node.parentNode.querySelectorAll('.photo')[0];
+    // if is a name anchor
+    if(photo){
+      node.parentNode.classList.add('clicked')
+      photo.addEventListener('animationend', () => {
+        window.location = url
+      })
+    }
+    else{
+      const main = document.querySelectorAll('.fade-up')[0];
+      main.addEventListener('animationend', () => {
+        window.location = url
+      })
+    }
+  }
+  node.addEventListener('click', delayVisit)
+})
