3d9a98ada44b6a385b70a620f51d73efe6cb9382
Walid <hello@walidvb.com>
2026-07-07 18:42:09 +0200

    no delay

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -2,19 +2,20 @@ var supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
 document.querySelector('html').classList.add(supportsTouch ? 'touch' : 'no-touch')
 const visiters = document.querySelectorAll('.visit-trigger');
 visiters.forEach(node => {
-  if(node.pathname != window.location.pathname){
-    const url = node.href;
+  if (node.pathname != window.location.pathname) {
+    return
+    const url = node.href
     const delayVisit = (evt) => {
       node.parentNode.classList.add('clicked')
-      evt.preventDefault();
-      document.body.classList.add('out');
-      
-      const fadeUps = document.querySelectorAll('.fade-up');
-      let fadedUp = 0;
-      fadeUps.forEach(elem => {
+      evt.preventDefault()
+      document.body.classList.add('out')
+
+      const fadeUps = document.querySelectorAll('.fade-up')
+      let fadedUp = 0
+      fadeUps.forEach((elem) => {
         elem.addEventListener('animationend', () => {
-          if (++fadedUp == fadeUps.length){
-            setTimeout(() => window.location = url, 50)
+          if (++fadedUp == fadeUps.length) {
+            setTimeout(() => (window.location = url), 50)
           }
         })
       })
